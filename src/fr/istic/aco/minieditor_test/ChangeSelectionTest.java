29a24081aafb9a3fd0190a35fa0ca69021df6008
mhiver <matthieu.hiver@etudiant.univ-rennes1.fr>
2014-12-03 12:23:22 +0100

    Début tests ChangeSelection

---
+++ src/fr/istic/aco/minieditor_test/ChangeSelectionTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package fr.istic.aco.minieditor_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author 12001247
+ *
+ */
+public class ChangeSelectionTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link fr.istic.aco.minieditor.ChangeSelection#ChangeSelection(fr.istic.aco.minieditor.EditorEngine, fr.istic.aco.minieditor.UI)}.
+	 */
+	@Test
+	public final void testChangeSelection() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link fr.istic.aco.minieditor.ChangeSelection#execute()}.
+	 */
+	@Test
+	public final void testExecute() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link fr.istic.aco.minieditor.ChangeSelection#getName()}.
+	 */
+	@Test
+	public final void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
